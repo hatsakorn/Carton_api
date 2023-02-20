efa05e809bd35077679a5042c70af559d2eef357
aisukdl <pk_aisu@hotmail.co.th>
2023-02-20 17:43:06 +0700

    feat: create login route

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 require('./config/passport')
 require('dotenv')
+
 const notFoundMiddleware = require("./middlewares/not-found");
 const errorMiddleware = require("./middlewares/error");
+const authRoute = require("./routes/auth-route")
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -10,6 +13,8 @@ const { sequelize } = require("./models");
 // sequelize.sync({force:true});
 app.use(cors());
 app.use(express.json());
+
+app.use("/auth", authRoute);
 
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
