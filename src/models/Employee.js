9ef7c1c02687e5797b186f53d0004b0105cb859b
pakinporing <pakinporing@gmail.com>
2023-02-20 18:40:12 +0700

    แก้login

--- src/models/Employee.js
+++ src/models/Employee.js
@@ -4,7 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   const Employee = sequelize.define(
     "Employee",
     {
-      name: {
+      firstName: {
         type: DataTypes.STRING,
         allowNull: false,
         validate: {
@@ -44,6 +44,13 @@ module.exports = (sequelize, DataTypes) => {
 
         allowNull: false,
         defaultValue: USER_EMPLOYEE
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
       }
     },
     { underscored: true, timestamps: false }
