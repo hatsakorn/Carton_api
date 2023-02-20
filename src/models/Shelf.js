6c3194575d1f7de4341c1f2eb9d71d3b0353a615
aisukdl <pk_aisu@hotmail.co.th>
2023-02-20 12:21:09 +0700

    add shelf model

---
+++ src/models/Shelf.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+    const Shelf = sequelize.define(
+      'Shelf',
+      {
+       isAvailable : {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue : false
+       }
+      },
+      {
+        underscored: true,
+        timestamps : false
+      }
+    );
+    return Shelf;
+  };
+  
