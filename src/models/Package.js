1b702d39f35af6d5c3ce67f47eb9d02cdd36b065
aisukdl <pk_aisu@hotmail.co.th>
2023-02-20 12:04:47 +0700

    add package model

---
+++ src/models/Package.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+    const Package = sequelize.define(
+      'Package',
+      {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+        description: DataTypes.STRING,
+        price: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        isActive: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false
+        },
+        startDate: DataTypes.DATE,
+        endDate: DataTypes.DATE,
+        posterUrl: DataTypes.STRING
+      },
+      {
+        underscored: true,
+        timestamps : false
+      }
+    );
+    return Package;
+  };
+  
