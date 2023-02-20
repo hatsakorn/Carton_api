f418113eb125f022ab42de38330b97efbd69a9d2
aisukdl <pk_aisu@hotmail.co.th>
2023-02-20 12:23:38 +0700

    add warehouse model

---
+++ src/models/Warehouse.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+    const Warehouse = sequelize.define(
+      'Warehouse',
+      {
+       location : {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty : true
+        }
+       }
+      },
+      {
+        underscored: true,
+        timestamps : false
+      }
+    );
+    return Warehouse;
+  };
+  
