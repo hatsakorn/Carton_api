783d3d05167ef2d10a5055ba1b6cdfd8fcc3ab25
aisukdl <pk_aisu@hotmail.co.th>
2023-02-20 17:34:38 +0700

    feat: add admin to passportauthen config

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,9 +1,11 @@
 const passport = require("passport");
+require('dotenv')
 const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt");
 const { Customer,Employee } = require("../models");
+const { USER_EMPLOYEE, USER_ADMIN } = require("../config/constant");
 
 let options = {
-  secretOrKey: "TheSecret",
+  secretOrKey: process.env.JWT_SECRET_KEY,
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 };
 
@@ -12,8 +14,10 @@ passport.use(
         try{
             const customer = await Customer.findOne({ where: {id: payload.id}}) 
             if(customer) return done(null,customer)
-            const employee = await Employee.findOne({ where: {id: payload.id}})
+            const employee = await Employee.findOne({ where: {id: payload.id,role: USER_EMPLOYEE}})
             if(employee) return done(null,employee)
+            const admin = await Employee.findOne({ where: {id: payload.id,role: USER_ADMIN}})
+            if(admin) return done(null,admin)
             return done(new Error("user not Found"), false)
         }catch(err) {
             return done(null,false);
