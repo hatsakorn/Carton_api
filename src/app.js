b4d5a65364efb41128e067f9fdb5d5b617cb6199
aisukdl <pk_aisu@hotmail.co.th>
2023-02-20 13:12:54 +0700

    add database config

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const errorMiddleware = require("./middlewares/error");
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const {sequelize} = require('./models');
 
+sequelize.sync({force:true});
 app.use(cors());
 app.use(express.json());
 
