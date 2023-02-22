a329f09238b6fbfff5df1763ff10ea56302b223d
aisukdl <pk_aisu@hotmail.co.th>
2023-02-22 14:51:48 +0700

    refactor : prettier

--- src/validators/auth-validators.js
+++ src/validators/auth-validators.js
@@ -44,14 +44,14 @@ const customerRegisterSchema = Joi.object({
     "string.empty": "lastName is required"
   }),
   confirmPassword: Joi.string()
-  .valid(Joi.ref('password'))
-  .required()
-  .trim()
-  .messages({
-    'any.only': 'password and confirm password did not match',
-    'string.empty': 'confirm password is required'
-  })
-  .strip()
+    .valid(Joi.ref("password"))
+    .required()
+    .trim()
+    .messages({
+      "any.only": "password and confirm password did not match",
+      "string.empty": "confirm password is required"
+    })
+    .strip()
 });
 
 exports.customerValidateRegister = validate(customerRegisterSchema);
