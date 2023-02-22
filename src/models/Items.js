7139b5b4b07943ae1823744a02808b6de85e039d
aisukdl <pk_aisu@hotmail.co.th>
2023-02-22 15:31:34 +0700

    fix: allownull to datein/out in items

--- src/models/Items.js
+++ src/models/Items.js
@@ -23,20 +23,8 @@ module.exports = (sequelize, DataTypes) => {
           notEmpty: true
         }
       },
-      dateIn: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notEmpty: true
-        }
-      },
-      dateOut: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notEmpty: true
-        }
-      }
+      dateIn: DataTypes.STRING,
+      dateOut: DataTypes.STRING,
     },
     { underscored: true, timestamps: false }
   );
