f639b28431dd7342332049529aae2528f0465af6
aisukdl <pk_aisu@hotmail.co.th>
2023-02-22 12:01:06 +0700

    refactor: with prettier

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,17 +1,17 @@
-const multer = require('multer');
+const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'public/images');
+    cb(null, "public/images");
   },
   filename: (req, file, cb) => {
     cb(
       null,
       new Date().getTime() +
-        '' +
+        "" +
         Math.round(Math.random() * 1000000000) +
-        '.' +
-        file.mimetype.split('/')[1]
+        "." +
+        file.mimetype.split("/")[1]
     );
   }
 });
