07b010d7cd991668b760f25ad56092753e651ec2
aisukdl <pk_aisu@hotmail.co.th>
2023-02-22 16:04:23 +0700

    fix: create invoice

--- src/controllers/invoice-controller.js
+++ src/controllers/invoice-controller.js
@@ -1,26 +1,35 @@
 const { Invoice, Items } = require("../models");
 exports.CreateInvoice = async (req, res, next) => {
   try {
-    console.log(req)
     const value = req.body;
     const customerId = req.user.id;
-    const newInvoice = await Invoice.create({ 
+    const newInvoice = await Invoice.create({
       customerId,
       status: "PENDING"
     });
     if (!newInvoice)
       return res.status(400).json({ error: "Falied to create invoice" });
     try {
-      const items = await Items.bulkCreate(value);
-      if(items==0) {
+      const itemsData = value.map((obj) => {
+        return {
+          invoiceId: newInvoice.id,
+          details: obj.details,
+          contractStartDate: obj.contractStartDate,
+          contractEndDate: obj.contractEndDate,
+          packageId: obj.packageId
+        };
+      });
+      console.log(itemsData);
+      const items = await Items.bulkCreate(itemsData);
+      if (items.length === 0) {
         await newInvoice.destroy();
-        return res.status(400).json({ error: "no item in invoice" });
+        return res.status(400).json({ error: "No items in invoice" });
       }
     } catch (err) {
       await newInvoice.destroy();
       return res.status(400).json({ error: "Failed to create items" });
     }
-    res.status(201).json({ message: "Reservation has created" });
+    res.status(201).json({ message: "Invoice has been created" });
   } catch (err) {
     next(err);
   }
