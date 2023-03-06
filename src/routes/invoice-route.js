b910db79c3933f3ce73117fe68f275e446ce526e
aisukdl <pk_aisu@hotmail.co.th>
2023-03-06 17:14:02 +0700

    fix : bug

--- src/routes/invoice-route.js
+++ src/routes/invoice-route.js
@@ -6,8 +6,10 @@ const router = express.Router();
 
 router.post("/create", passportAuthen, invoiceController.CreateInvoice);
 router.get("/", passportAuthen, invoiceController.GetInvoiceByUserId);
+router.post("/qrcode",invoiceController.getItemsLocationByItemIdAndCustomerId);
 router.post("/:invoiceId", invoiceController.Omise);
 router.get("/:customerId", invoiceController.getInvoiceById);
 router.get("/", invoiceController.getAllInvoiceByAdmin);
+
 
 module.exports = router;
