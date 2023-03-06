3a9977d866320eb0f043e5fe37efd8e497e51c8d
pakinporing <pakinporing@gmail.com>
2023-03-06 13:37:03 +0700

    แก้พาสปอตไม่ให้ส่งค่าพาสเวิด

--- src/config/passport.js
+++ src/config/passport.js
@@ -13,7 +13,10 @@ passport.use(
   new JwtStrategy(options, async (payload, done) => {
     try {
       const customer = await Customer.findOne({
-        where: { id: payload.id, username: payload.username }
+        where: { id: payload.id, username: payload.username },
+        attributes: {
+          exclude: ["password"]
+        }
       });
       if (customer) return done(null, customer);
       const employee = await Employee.findOne({
@@ -21,11 +24,17 @@ passport.use(
           id: payload.id,
           username: payload.username,
           role: USER_EMPLOYEE
+        },
+        attributes: {
+          exclude: ["password"]
         }
       });
       if (employee) return done(null, employee);
       const admin = await Employee.findOne({
-        where: { id: payload.id, username: payload.username, role: USER_ADMIN }
+        where: { id: payload.id, username: payload.username, role: USER_ADMIN },
+        attributes: {
+          exclude: ["password"]
+        }
       });
       if (admin) return done(null, admin);
       return done(new Error("user not Found"), false);
